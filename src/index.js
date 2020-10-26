7f4479d72d06c447b056df4ed76d934993632043
scottwcode <scottmail77@gmail.com>
2020-10-26 17:00:16 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import  mongoose from "mongoose";
+import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./typeDefs.js";
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app });
+
+ await mongoose.connect("mongodb+srv://scott1234:scott1234@cluster0.oc6bf.mongodb.net/react-eccommerce-site?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+  });
+  
+  // await mongoose.connect("mongodb://localhost:27017/test3", {
+  //   useNewUrlParser: true
+  // });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
