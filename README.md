3abc0339edfbe72710c38f5824847d9157767b3e
scottwcode <scottmail77@gmail.com>
2020-10-28 10:10:38 -0400

    did some clean-up

---
+++ README.md
@@ -0,0 +1,34 @@
+Basic GraphQL server using MongoDB
+
+Pre-Reqs
+-node to be able to run npm or yarn to install dependencies and run a server
+-github to be able to clone/copy repo
+-Need MongoDB account and cluster created to be able to add collections (free tier should be fine)
+
+Also uses the following dependencies via npm install (or yarn add)
+mongoose - used for MongoDB connectivity
+nodemon - to auto-restart node server when changes are made
+dotenv - used for environment vars.
+
+To set up the project
+Step 1: Clone or Copy the repo
+
+git clone https://github.com/scottwcode/mongodb-graphql-server
+
+Step 2: cd into your local cloned repo and run:
+
+npm install
+Step 3: Create an .env file and add your connectivity info in it
+
+PORT=4000
+HOST=localhost
+MONGO_USER=<YOUR-MONGO-USER-ID>
+MONGO_PASS=<YOUR-MONGO-USER-PASSWD>
+MONGO_DB=<MONGO-DB-YOU-WANT-TO-USE>
+Step 4: Start the API by running the following
+
+npm start
+Author
+ScottW
+License
+This project is licensed under the MIT License.
--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import { resolvers } from "./resolvers.js";
 import { typeDefs } from "./typeDefs.js";
 
 dotenv.config({ debug: process.env.DEBUG })
-// console.log(process.env.MONGO_USER);
 
+// Destructuring env vars to be used later
+const {HOST, PORT, MONGO_DB, MONGO_USER, MONGO_PASS} = process.env
+// console.log(HOST, PORT, MONGO_DB, MONGO_USER, MONGO_PASS);
 
 const startServer = async () => {
   const app = express();
@@ -20,13 +22,17 @@ const startServer = async () => {
   server.applyMiddleware({ app });
 
 
-   await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.oc6bf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+   await mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.oc6bf.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true
   });
 
+  //    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.oc6bf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+  //   useNewUrlParser: true
+  // });
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  // app.listen({ port: 4000 }, () =>
+    app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://${HOST}:${PORT}${server.graphqlPath}`)
   );
 };
 
