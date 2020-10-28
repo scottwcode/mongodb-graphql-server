808f30453ac937ad79fb59f31f26cd5a6429c3ee
scottwcode <scottmail77@gmail.com>
2020-10-28 09:35:24 -0400

    config testing and troubleshooting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// require('dotenv').config();
 import dotenv from 'dotenv';
 import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
@@ -6,7 +5,9 @@ import  mongoose from "mongoose";
 import { resolvers } from "./resolvers.js";
 import { typeDefs } from "./typeDefs.js";
 
-dotenv.config();
+dotenv.config({ debug: process.env.DEBUG })
+// console.log(process.env.MONGO_USER);
+
 
 const startServer = async () => {
   const app = express();
@@ -18,21 +19,11 @@ const startServer = async () => {
 
   server.applyMiddleware({ app });
 
- await mongoose.connect("mongodb+srv://scott1234:scott1234@cluster0.oc6bf.mongodb.net/react-eccommerce-site?retryWrites=true&w=majority", {
+
+   await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.oc6bf.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true
   });
 
-  //  await mongoose.connect(`${process.env.MONGO_URI}`, {
-  //   useNewUrlParser: true
-  // });
-
-  //  await mongoose.connect(process.env.MONGO_URI, {
-  //   useNewUrlParser: true
-  // });
-  
-  // await mongoose.connect("mongodb://localhost:27017/test3", {
-  //   useNewUrlParser: true
-  // });
 
   app.listen({ port: 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
